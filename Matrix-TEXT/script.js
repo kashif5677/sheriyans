33c6484dd09d1a885f5bdd6ecb785ed6a2842c28
kashif5500 <mdkashifnisar5677@gmail.com>
2026-01-17 22:26:46 +0530

    Add new folder

--- Matrix-TEXT/script.js
+++ Matrix-TEXT/script.js
@@ -2,18 +2,32 @@ const para = document.body.querySelector('p')
 const charcter = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijhklmnopqrstuvwxyz"
 const text = para.innerText
 
-para.addEventListener("mouseenter", (e) => {
-
-    setInterval(() => {
-
-        const str = text.split('').map((char, index) => {
-            return charcter.split("")[Math.floor(Math.random() * 53)]
-        }).join("")
-        para.innerText = str
-    }, 30)
+// para.addEventListener("mouseenter", (e) => {
 
 
 
+let interation = 0
+
+function randomText() {
+
+    const str = text.split('').map((char, index) => {
+
+        if (index < interation) {
+            return char
+        }
+
+        return charcter.split('')[Math.floor(Math.random() * 52)]
+    }).join("")
 
 
-})
+    // console.log(str);
+
+    para.innerText = str
+    interation += 0.7
+
+}
+
+
+
+setInterval(randomText, 30)
+// })
