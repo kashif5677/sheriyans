585707e804799f7299c11d8b17c322f5fb1ef14e
kashif5500 <mdkashifnisar5677@gmail.com>
2026-01-23 01:24:06 +0530

    more on ReactDOM

--- React_Element/script.js
+++ React_Element/script.js
@@ -1,12 +1,16 @@
 
 
-let h1 = React.createElement('h1', null, "hello form react")
+let h1 = React.createElement('h1', { id: 'hero' }, "this is h1")
+let h2 = React.createElement('h2', { id: "queen" }, "this is h2")
+
+
+let div = React.createElement('div', { id: "parent" }, [h1, h2])
 
 let container = document.querySelector("#root")
 
 let root = ReactDOM.createRoot(container)
 
-root.render(h1)
+root.render(div)
 
 
 
