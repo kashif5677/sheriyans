ea0f9e342b89a24c180859996fd01612cea6c721
kashif5500 <mdkashifnisar5677@gmail.com>
2026-01-23 01:07:09 +0530

    React DOM

---
+++ React_Element/script.js
@@ -0,0 +1,12 @@
+
+
+let h1 = React.createElement('h1', null, "hello form react")
+
+let container = document.querySelector("#root")
+
+let root = ReactDOM.createRoot(container)
+
+root.render(h1)
+
+
+
